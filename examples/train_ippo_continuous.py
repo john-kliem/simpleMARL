Fixed unwanted gradient trackign caused by not having with torch.no_grad() on a get_value() call

examples/train_ippo_continuous.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
             policies[aid] = ppo.PPO(obs_spaces[aid], act_spaces[aid], config)
         elif args.policies[aid] == "init_ppo_continuous":
             config = ppo.PPOConfig()
-            config.ent_coef = 0.1
+            config.ent_coef = 0.0
             config.learning_rate = 1.5e-4
             config.num_iterations = args.num_iterations 
             config.device = args.device
@@ -201,7 +201,8 @@ if __name__ == "__main__":
                 buffers[aid].step()
         #Bootstrap values in all buffers GAE
         for aid in buffers:
-            next_value = policies[aid].get_value(torch.from_numpy(rets[aid]['obs'])).squeeze(-1)
+            with torch.no_grad():
+                next_value = policies[aid].get_value(torch.from_numpy(rets[aid]['obs'])).squeeze(-1)
             next_done = torch.from_numpy(np.logical_or(rets[aid]["terms"], rets[aid]["truncs"]).astype(np.float32))
             buffers[aid].returns_and_advantages(next_value, next_done)#calculate_returns_and_advantages(policies[aid].config.gamma, policies[aid].config.gae_lambda)
 

simplemarl/algorithms/ppo.py
@@ -51,7 +51,7 @@ class PPOConfig:
     """the surrogate clipping coefficient"""
     clip_vloss: bool = True
     """Toggles whether or not to use a clipped loss for the value function, as per the paper."""
-    ent_coef: float = 0.1#Maybe should be 0.1 default 0.01
+    ent_coef: float = 0.1
     """coefficient of the entropy"""
     vf_coef: float = 0.5
     """coefficient of the value function"""
@@ -87,6 +87,7 @@ class PPO(nn.Module):
             nn.Tanh(),
             layer_init(nn.Linear(64,1), std=1.0),
         )
+
         self.actor = nn.Sequential(
             layer_init(nn.Linear(np.array(obs_space.shape).prod(), 64)),
             nn.Tanh(),